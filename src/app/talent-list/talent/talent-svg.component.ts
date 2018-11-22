7401b0bbaf2573410d5fde1e859f26e1fc0949f7
Gal Appelbaum <galappelbaum@gmail.com>
2018-11-22 21:22:34 +0200

    test first push

--- src/app/talent-list/talent/talent-svg.component.ts
+++ src/app/talent-list/talent/talent-svg.component.ts
@@ -1,16 +1,16 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'app-talent-svg',
-    templateUrl: 'talent-svg.component.html'
+  selector: 'app-talent-svg',
+  templateUrl: 'talent-svg.component.html'
 })
 export class TalentSVGComponent {
-    public imagePath = '';
+  public imagePath = '';
 
-    @Input()
-    set image(image: string) {
-        this.imagePath = `talent-list/talent/assets/images/${image}.png`;
-    }
+  @Input()
+  set image(image: string) {
+    this.imagePath = `talent-list/talent/assets/images/${image}.png`;
+  }
 
-    constructor() { }
+  constructor() {}
 }
--- src/app/talent-list/talent/talent.model.ts
+++ src/app/talent-list/talent/talent.model.ts
@@ -1,19 +1,21 @@
 export type TalentArrayType = Map<string, TalentModel>;
 
 export class TalentModel {
-    constructor(
-        public readonly name: string = '', 
-        public readonly description: string = '',
-        public readonly values: Array<Array<number>> = [],
-        public readonly required: Array<string> = [],
-        public readonly coordinate: any = {},
-        public readonly type: string = 'Buff',
-        public level: number = 0,
-        public totalLevel: number = 0,
-        public readonly lines: Array<any> = [],
-        public readonly dependencies: Array<string> = [],
-        public readonly isSpecial: boolean = false
-    ) {
-    }
+  constructor(
+    public readonly name: string = '',
+    public readonly title: string = '',
+    public readonly description: string = '',
+    public readonly imageName: string = '',
+    public readonly values: Array<Array<number>> = [],
+    public readonly required: Array<string> = [],
+    public readonly coordinate: any = {},
+    public readonly type: string = 'Buff',
+    public level: number = 0,
+    public totalLevel: number = 0,
+    public readonly lines: Array<any> = [],
+    public readonly dependencies: Array<string> = [],
+    public readonly dependenciesTitles: Array<string> = [],
+    public readonly size: string = '',
+    public readonly isSpecial: boolean = false
+  ) {}
 }
-
